1ba4d11f84f947469c117fc96079cb10d9150eff
frontdevuz <davlatovizzatillo7@gmail.com>
2026-04-08 16:27:13 +0500

    Starting

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>G'ofurova Nigora </h1>
+      <h2>Web Designer</h2>
+      <p>Hello, I'm Nigora! I'm a passionate web designer.</p>
+    </>
+  )
+}
+
+export default App
